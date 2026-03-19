5dedfec4ff16e16a0bf3d2bdc747a9c1a89be5cd
cponce123com-create <cponce123.com@gmail.com>
2026-03-19 14:21:27 -0500

    Update index.ts

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router, type IRouter } from "express";
 import healthRouter from "./health";
 import authRouter from "./auth";
+import storesRouter from "./stores";
 
 const router: IRouter = Router();
 
 router.use(healthRouter);
 router.use("/auth", authRouter);
+router.use("/stores", storesRouter);
 
 export default router;
